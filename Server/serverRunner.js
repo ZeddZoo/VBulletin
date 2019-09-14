65d046067d40f77c9441edb8f5a50a984fc5f433
ZeddZoo <ziyizuo@andrew.cmu.edu>
2019-09-13 21:48:38 -0400

    Created first test board. Setting up request framework

--- Server/serverRunner.js
+++ Server/serverRunner.js
@@ -1,12 +1,29 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
+const fs = require('fs');
+
+var clientCount = 0;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  // Read Request
+  if (req.method === 'GET') {
+    const requestedBoard =
+  // Write Request
+  } else if (req.method === 'POST') {
+
+  }
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.write("Welcome to the VBulletin server!\n");
+  // res.write("" + clientCount);
+  res.end();
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  clientCount++;
+
 });
+
+// const getBulletin = (bulletinId) =>
