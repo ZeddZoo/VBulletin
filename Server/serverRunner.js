a2ed89fb426312750452e2572308f75f8a98acd5
ZeddZoo <ziyizuo@andrew.cmu.edu>
2019-09-13 22:14:17 -0400

    Wrote GET reply code

--- Server/serverRunner.js
+++ Server/serverRunner.js
@@ -8,8 +8,35 @@ var clientCount = 0;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   // Read Request
+  var requestBody = "";
+  // From https://stackoverflow.com/questions/31006711/get-request-body-from-node-jss-http-incomingmessage
+  req.on('readable', () => {
+    requestBody += req.read();
+  });
+
+  var requestBody = JSON.parse(requestBody);
+
   if (req.method === 'GET') {
-    const requestedBoard =
+    const reqBoardNum = requestBody[board];
+    const username = requestBody[username];
+
+    const jsonRequest = "Boards/" + "board" + reqBoardNum + ".JSON";
+
+    var result = $.getJSON(jsonRequest, () => {
+      console.log("Getting board: " + jsonRequest);
+    })();
+
+    if (result != null) {
+      var replyBody = {
+        "boardId": reqBoardNum,
+        "board": JSON.stringify(result)
+      };
+      res.write(JSON.stringify(replyBody));
+    } else {
+      res.statusCode = 418;
+      res.write("Board not found :(");
+    }
+
   // Write Request
   } else if (req.method === 'POST') {
 
