558aab65e199130acc0640c604cbc1f086c005c2
hostilechild007 <47698870+hostilechild007@users.noreply.github.com>
2019-09-14 16:08:30 -0400

    Add files via upload
    Users can type what they want in a textbox and it uploads live what they text into camera file

---
+++ bull-e10/components/Camera.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera, Permissions } from 'expo';
+import Customize from './Customize';
+import GLOBALS from './global';
+
+
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                justifyContent: 'flex-end', alignSelf: 'flex-end'
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.05,
+                  
+                }}
+                onPress={() => {
+                  this.setState({
+                    type:
+                      this.state.type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
+  }
+}
+
---
+++ bull-e10/components/Customize.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Alert,
+  ScrollView,
+  Dimensions, SafeAreaView
+
+  } from 'react-native';
+  
+
+import { Dropdown } from 'react-native-material-dropdown';
+import GLOBALS from './global';
+
+
+export default class HelloWorldApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '', height: 0};
+  }
+  render() {
+    GLOBALS.userInput = this.state.text;
+    return (
+      <View style= {{flex: 1, justifyContent: "center"}}>      
+        <TextInput
+          placeholder="Write something with multilines  here.."
+
+          {...this.props}
+          multiline={true}
+          onChangeText={(text) => {
+              this.setState({ text })
+          }}
+          onContentSizeChange={(event) => {
+              this.setState({ height: event.nativeEvent.contentSize.height })
+          }}
+          style={[styles.default, {height: Math.max(35, this.state.height)}], styles.bioBlock}
+          value={this.state.text}
+        />
+        //<Text> {GLOBALS.userInput} </Text> 
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bioBlock: {
+      paddingHorizontal: '5%', 
+      borderRadius: 10, 
+      borderWidth: 1,
+      borderColor: '#f0f8ff',
+      justifyContent: 'flex-start',
+
+    },
+  submit:{
+      width: '50%', 
+      justifyContent: 'center', 
+      borderRadius: 50, 
+      margin: 20,
+      backgroundColor: '#e1665c',
+      fontWeight: 'bold',
+      
+  }
+
+});
